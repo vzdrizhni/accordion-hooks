4257e9ddb91738797f9676a788285a4df2f1212a
Danial41 <vzdrizhni@gmail.com>
2021-07-30 14:05:19 +0500

    Show object onclick

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
@@ -35,11 +35,17 @@ const options = [
 ];
 
 export default () => {
+  const [selected, setSelected] = useState(options[0]);
+
   return (
     <div>
       {/* <Accordion items={items} /> */}
       {/* <Search /> */}
-      <Dropdown options={options} />
+      <Dropdown
+        selected={selected}
+        onSelectedChange={setSelected}
+        options={options}
+      />
     </div>
   );
 };
