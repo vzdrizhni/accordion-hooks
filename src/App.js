07dca3a4331b8bbbbe21a4ca0e1092fc53a5e7a5
Roman <vzdrizhni@gmail.com>
2021-08-23 16:21:17 +0500

    Add route component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
 import Translate from "./components/Translate";
+import Route from "./components/Route";
+import Header from "./components/Header";
 
 const items = [
   {
@@ -36,8 +38,8 @@ const options = [
 ];
 
 const showAccordion = () => {
-  if (window.location.pathname === '/') return <Accordion items={items} />
-}
+  if (window.location.pathname === "/") return <Accordion items={items} />;
+};
 
 const showList = () => {
   if (window.location.pathname === "/list") {
@@ -63,11 +65,24 @@ export default () => {
 
   return (
     <div>
-      {showAccordion()}
-      {showList()}
-      {showTranslate()}
-      {showDropdown()}
-      <Translate />
+      <Header />
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown
+          label="Select a color"
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
     </div>
   );
 };
